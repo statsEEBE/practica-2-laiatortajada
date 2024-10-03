1a121a0e00b508a580885ed9e270b33a880ac057
laia.tortajada <laia.tortajada@auleseebe.upc.edu>
2024-10-03 11:29:02 +0200

    prac2

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,15 @@
-#Codigo para problema 1
+#coeficient de determinació = R^2
+#r^2= 0,1923 (agafat del document)
 
+#coeficient de correlació= sqrt(R^2)
+
+#variació del preu del mercat= pendent
+#estimate x= estimate assets
+
+x<- 647.1
+m<- 0.06576
+b<- 11.79755
+y_pred<- m*x+b
+
+#per trobar el gràfic, cal mirar el valor de
+#R^2 u depen de la dispercio de les dades
